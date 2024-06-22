fe63c2784db7133e5ff98a0236bd12e70c7f7ab4
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-06-22 22:49:34 +0600

    deployed to verce

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://tenantix.web.app", "https://tenantix.firebaseapp.com"],
+  })
+);
 app.use(express.json());
 const verifyToken = (req, res, next) => {
   console.log("in verify token", req.headers?.authorization);
@@ -44,7 +48,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const roomsCollection = client.db("tenantixDB").collection("rooms");
     const couponsCollection = client.db("tenantixDB").collection("coupons");
@@ -355,8 +359,8 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
